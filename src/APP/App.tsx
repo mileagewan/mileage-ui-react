4507f8f2391a062edfccc244bbd0ca4bbce6c2a0
mileage <792668520@qq.com>
2021-01-24 22:43:12 +0800

    【story开发/问题单号】：新开发 【作者】：mileage 【修改详情】：添加less

--- src/APP/App.tsx
+++ src/APP/App.tsx
@@ -24,20 +24,41 @@
 // }
 import * as React from 'react';
 import { MileageButtonReact } from '../package/index';
+import 'style-loader!css-loader!less-loader!./App.less';
+const marked = require('marked');
+const test = require('./App.md');
 export class App extends React.Component {
   state: { text: string };
+  renderer: any;
+  components: Map<number, HTMLElement> = new Map();
   constructor(props: Validation.ButtonState) {
     super(props);
+    this.renderer = new marked.Renderer();
     this.state = {
       text: '按钮',
     };
   }
   render(): JSX.Element {
-    return <MileageButtonReact text="213" />;
+    const _this = this;
+    const __html = marked(
+      test.replace(
+        /:::[^]+demo[^]+```js([^]+)```[^]+:::/,
+        function (match: string, p1: string, offset: number) {
+          _this.components.set(String(offset), React.createElement());
+        }
+      ),
+      {
+        renderer: this.renderer,
+      }
+    );
+    // return <MileageButtonReact text="213" />;
+    return <div dangerouslySetInnerHTML={{ __html: marked(test) }}></div>;
   }
 }
 declare global {
-  interface Window { App: any; }
+  interface Window {
+    App: any;
+  }
 }
 
-window.App = App
+window.App = App;
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,7 @@
+import { any } from 'prop-types';
+
 declare module '*.css';
 declare module '*.less';
 declare module '*.scss';
 declare module '*.sass';
+declare var marked: any;
